6f824c4d20199144b3052ee59b6df53025a6362a
AH72 <abilashkj072@gmail.com>
2020-10-25 00:36:50 +0530

    added values Validation!

--- app/src/main/java/com/aj/android/CreditCardInputDemo/MainActivity.java
+++ app/src/main/java/com/aj/android/CreditCardInputDemo/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,107 +11,62 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
-import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
+    private TextInputEditText inputCardNum;
+    private TextInputEditText inputExpiryDate;
     private TextInputEditText inputCode;
+    private TextInputEditText inputFirstName;
+    private TextInputEditText inputLastName;
+    private TextInputLayout cardNumberLayout;
+    private TextInputLayout expiryDateLayout;
+    private TextInputLayout securityCodeLayout;
+    private TextInputLayout firstNameLayout;
+    private TextInputLayout lastNameLayout;
+
+    private Button submit;
     private String firstName, lastName, expiryDate, codeType;
-    private char[] cardNumber,securityCode;
-    private int codeLength = 3,cardNumCount;
-    private boolean isNameValid=false,isCodeValid=false,isExpDateValid=false;
+    private char[] cardNumber, securityCode;
+    private int codeLength = 3, cardNumCount;
+    private boolean isNameValid = false, isCodeValid = false, isExpDateValid = false, isCardValid = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardNumberLayout=findViewById(R.id.cardNumberLayout);
+        inputCardNum = findViewById(R.id.cardNumber);
+
+        expiryDateLayout = findViewById(R.id.expiryDateLayout);
+        inputExpiryDate = findViewById(R.id.expiryDate);
+
+        securityCodeLayout=findViewById(R.id.securityCodeLayout);
+        inputCode = findViewById(R.id.securityCode);
+
+        firstNameLayout=findViewById(R.id.firstNameLayout);
+        inputFirstName = findViewById(R.id.firstName);
+
+        lastNameLayout=findViewById(R.id.lastNameLayout);
+        inputLastName = findViewById(R.id.lastName);
+
+        submit = findViewById(R.id.submitPay);
+        submit.setOnClickListener(this);
+
         cardNumberReader();
         expiryDateReader();
         firstNameReader();
         lastNameReader();
         securityCodeReader();
-        submitPayButton();
 
 
-    }
-
-    private void submitPayButton() {
-        Button submit = findViewById(R.id.submitPay);
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (luhnTest(cardNumber))
-                    Toast.makeText(MainActivity.this, "Payment Successful", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private boolean luhnTest(char[] num) {
-        long sum = 0, d, m = 1;
-        for (int i = num.length - 1; i >= 0; i--) {
-            d = m * Integer.parseInt(String.valueOf(num[i]));
-            sum = sum + d;
-            m = m == 2 ? 1 : 2;
-        }
-        return sum % 10 == 0;
-    }
-
-    private void firstNameReader() {
-        TextInputEditText inputFirstName = findViewById(R.id.firstName);
-        inputFirstName.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (!isValid(s.toString()))
-                    inputFirstName.setError("First Name can only contains alphabets and spaces");
-                firstName = s.toString();
-            }
-        });
-    }
-
-    private void lastNameReader() {
-        TextInputEditText inputLastName = findViewById(R.id.lastName);
-        inputLastName.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (!isValid(s.toString()))
-                    inputLastName.setError("First Name can only contains alphabets and spaces");
-                lastName = s.toString();
-            }
-        });
-    }
-
-    private boolean isValid(String s) {
-        boolean valid = true;
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            if (!Character.isAlphabetic(current) && current != ' ')
-                valid = false;
-        }
-        return valid;
     }
 
     private void securityCodeReader() {
-        inputCode = findViewById(R.id.securityCode);
         changeSecurityCodeType();
         String null_val = "";
 
@@ -121,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
             public void onFocusChange(View v, boolean hasFocus) {
                 inputCode.setHint(hasFocus ? codeType : null_val);
                 securityCode = Objects.requireNonNull(inputCode.getText()).toString().toCharArray();
+                isCodeValid = securityCode != null && codeLength == securityCode.length;
             }
         });
     }
@@ -136,20 +91,20 @@ public class MainActivity extends AppCompatActivity {
         final int DIVIDER_POS = 4;
         final int DIVIDER_MOD = DIVIDER_POS + 1;
         final char DIVIDER_CHAR = ' ';
-        TextInputEditText inputCardNum = findViewById(R.id.cardNumber);
+
         inputCardNum.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
 
                 inputCardNum.setHint(hasFocus ? R.string.card_hint : R.string.null_val);
-                if (cardNumber!=null)
-                if (cardNumber.length==15) {
-                    codeLength = 4;
-                    changeSecurityCodeType();
-                }else if (cardNumber.length==16) {
-                    codeLength = 3;
-                    changeSecurityCodeType();
-                }
+                if (cardNumber != null)
+                    if (cardNumber.length == 15) {
+                        codeLength = 4;
+                        changeSecurityCodeType();
+                    } else if (cardNumber.length == 16) {
+                        codeLength = 3;
+                        changeSecurityCodeType();
+                    }
             }
         });
         inputCardNum.addTextChangedListener(new TextWatcher() {
@@ -164,8 +119,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 cardNumber = getCardNum(s, DIGIT_COUNT);
-                Log.d(TAG, "afterTextChanged: "+ cardNumber.length);
-                if (!isCorrectStr(s, TOTAL_SIZE, DIVIDER_MOD, DIVIDER_CHAR)) {
+                if (isCorrectStr(s, TOTAL_SIZE, DIVIDER_MOD, DIVIDER_CHAR) != -1) {
                     s.replace(0, s.length(), getCorrectString(cardNumber, DIVIDER_CHAR, DIVIDER_POS));
                 }
             }
@@ -177,11 +131,23 @@ public class MainActivity extends AppCompatActivity {
         final int DIVIDER_POS = 2;
         final int DIVIDER_MOD = DIVIDER_POS + 1;
         final char DIVIDER_CHAR = '/';
-        TextInputEditText inputExpiryDate = findViewById(R.id.expiryDate);
         inputExpiryDate.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                inputExpiryDate.setHint(getString(hasFocus ? R.string.expiry_date : R.string.expiry));
+                expiryDateLayout.setHint(getString(hasFocus ? R.string.expiry_date : R.string.expiry));
+                if (!hasFocus && expiryDate != null) {
+                    String[] date = expiryDate.split("/");
+                    if (date.length==2) {
+                        int m = Integer.parseInt(date[0]), y = Integer.parseInt(date[1]);
+                        int currentMonth = Calendar.getInstance().get(Calendar.MONTH), currentYear = Calendar.getInstance().get(Calendar.YEAR);
+                        if (m > 12 || m < 0) {
+                            expiryDateLayout.setError("Invalid Date");
+                        } else if ((m < currentMonth + 1 && y == currentYear % 100) || y < currentYear % 100) {
+                            expiryDateLayout.setError("Card Expired!");
+                        } else
+                            isExpDateValid = true;
+                    }
+                }
             }
         });
         inputExpiryDate.addTextChangedListener(new TextWatcher() {
@@ -195,24 +161,26 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                char[] expDate = getCardNum(s, DIGIT_COUNT);
-                expiryDate = Arrays.toString(expDate);
-                if (!isCorrectStr(s, DIGIT_COUNT, DIVIDER_MOD, DIVIDER_CHAR)) {
-                    s.replace(0, s.length(), getCorrectString(expDate, DIVIDER_CHAR, DIVIDER_POS));
+                expiryDate = s.toString();
+                if ((cardNumCount = isCorrectStr(s, DIGIT_COUNT, DIVIDER_MOD, DIVIDER_CHAR)) != -1) {
+                    s.replace(0, s.length(), getCorrectString(getCardNum(s, DIGIT_COUNT), DIVIDER_CHAR, DIVIDER_POS));
                 }
             }
         });
     }
 
-    public boolean isCorrectStr(Editable s, int totalSymbols, int dividerPos, char dividerChar) {
+    public int isCorrectStr(Editable s, int totalSymbols, int dividerPos, char dividerChar) {
         boolean isCorrect = s.length() <= totalSymbols;
+        int count = 0;
         for (int i = 0; i < s.length(); i++) {
             if (i > 0 && (i + 1) % dividerPos == 0)
                 isCorrect &= dividerChar == s.charAt(i);
-            else
+            else {
+                count++;
                 isCorrect &= Character.isDigit(s.charAt(i));
+            }
         }
-        return isCorrect;
+        return isCorrect ? -1 : count;
     }
 
     public String getCorrectString(char[] digits, char dividerChar, int dividerPos) {
@@ -237,5 +205,92 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return digits;
+    }
+
+    private void firstNameReader() {
+        inputFirstName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!(isNameValid = isValid(s.toString())))
+                    firstNameLayout.setError("First Name can only contains alphabets and spaces");
+                firstName = s.toString();
+            }
+        });
+    }
+
+    private void lastNameReader() {
+
+        inputLastName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!(isNameValid = isValid(s.toString())))
+                    lastNameLayout.setError("First Name can only contains alphabets and spaces");
+                lastName = s.toString();
+            }
+        });
+    }
+
+    private boolean isValid(String s) {
+        boolean valid = true;
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (!Character.isAlphabetic(current) && current != ' ')
+                valid = false;
+        }
+        return valid;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (cardNumber==null)
+            cardNumberLayout.setError("can't be Empty");
+        else if (!luhnTest(cardNumber))
+            cardNumberLayout.setError("Invalid Credit Card Number");
+        else
+            isCardValid=true;
+
+        if (expiryDate==null)
+            expiryDateLayout.setError("can't be Empty");
+
+        if (firstName==null)
+            firstNameLayout.setError("can't be Empty");
+        if (lastName==null)
+            lastNameLayout.setError("can't be Empty");
+
+        if (securityCode==null)
+            securityCodeLayout.setError("can't be Empty");
+        else if (!isCodeValid)
+            securityCodeLayout.setError("Please Enter " + codeLength + " digits code");
+
+        if (isCardValid&&isCodeValid&&isNameValid&&isExpDateValid)
+            Toast.makeText(this, "Payment Successful!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean luhnTest(char[] num) {
+        long sum = 0, d, m = 1;
+        for (int i = cardNumCount; i >= 0; i--) {
+            d = m * Integer.parseInt(String.valueOf(num[i]));
+            sum = sum + d;
+            m = m == 2 ? 1 : 2;
+        }
+        return sum % 10 == 0;
     }
 }
